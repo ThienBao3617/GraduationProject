47e17f6929b9d2e97ad030062048530373645d22
ThienBao3617 <nltb12042003@gmail.com>
2026-01-02 17:12:59 +0700

    adding noise to robot motion and launch noisy controller and using plotjuggler tool to visulize graphic of odometry comparison

--- workspace/amr_ws/src/amr_controller/launch/controller.launch.py
+++ workspace/amr_ws/src/amr_controller/launch/controller.launch.py
@@ -1,8 +1,43 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.actions import DeclareLaunchArgument, GroupAction, OpaqueFunction
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 from launch.conditions import UnlessCondition, IfCondition
+
+
+def noisy_controller(context, *args, **kwargs):
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    use_python = LaunchConfiguration("use_python")
+    wheel_radius = float(LaunchConfiguration("wheel_radius").perform(context))
+    wheel_separation = float(LaunchConfiguration("wheel_separation").perform(context))
+    wheel_radius_error = float(LaunchConfiguration("wheel_radius_error").perform(context))
+    wheel_separation_error = float(LaunchConfiguration("wheel_separation_error").perform(context))
+
+    noisy_controller_py = Node(
+        package="amr_controller",
+        executable="noisy_controller.py",
+        parameters=[
+            {"wheel_radius": wheel_radius + wheel_radius_error,
+             "wheel_separation": wheel_separation + wheel_separation_error,
+             "use_sim_time": use_sim_time}],
+        condition=IfCondition(use_python),
+    )
+
+    noisy_controller_cpp = Node(
+        package="amr_controller",
+        executable="noisy_controller",
+        parameters=[
+            {"wheel_radius": wheel_radius + wheel_radius_error,
+             "wheel_separation": wheel_separation + wheel_separation_error,
+             "use_sim_time": use_sim_time}],
+        condition=UnlessCondition(use_python),
+    )
+
+    return [
+        noisy_controller_py,
+        noisy_controller_cpp,
+    ]
+
 
 def generate_launch_description():
     use_sim_time_arg = DeclareLaunchArgument(
@@ -13,6 +48,10 @@ def generate_launch_description():
         "use_python",
         default_value="False",
     )
+    use_simple_controller_arg = DeclareLaunchArgument(
+        "use_simple_controller",
+        default_value="True",
+    )
     wheel_radius_arg = DeclareLaunchArgument(
         "wheel_radius",
         default_value="0.033",
@@ -21,9 +60,13 @@ def generate_launch_description():
         "wheel_separation",
         default_value="0.17",
     )
-    use_simple_controller_arg = DeclareLaunchArgument(
-        "use_simple_controller",
-        default_value="True",
+    wheel_radius_error_arg = DeclareLaunchArgument(
+        "wheel_radius_error",
+        default_value="0.005",
+    )
+    wheel_separation_error_arg = DeclareLaunchArgument(
+        "wheel_separation_error",
+        default_value="0.02",
     ) 
 
     use_sim_time = LaunchConfiguration("use_sim_time") 
@@ -84,15 +127,20 @@ def generate_launch_description():
         ]
     )
 
+    noisy_controller_launch = OpaqueFunction(function=noisy_controller)
+
     return LaunchDescription(
         [
             use_sim_time_arg,
             use_python_arg,
+            use_simple_controller_arg,
             wheel_radius_arg,
             wheel_separation_arg,
-            use_simple_controller_arg,
+            wheel_radius_error_arg,
+            wheel_separation_error_arg,
             joint_state_broadcaster_spawner,
             wheel_controller_spawner,
             simple_controller,
+            noisy_controller_launch,
         ]
     )
